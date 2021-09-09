67b98a0cf998829671379b79cfdadb526289705b
Naomi Mango <naomi.mango@gmail.com>
2021-09-09 19:12:37 +0200

    [MAJ] Navbar, design responsive, transitions, animations

--- js/index.js
+++ js/index.js
@@ -9,3 +9,14 @@ var docWidth = document.documentElement.offsetWidth;
         }
     }
 );
+
+const links = document.links;
+window.addEventListener('scroll', function(e)  {
+    for (let index = 0; index < links.length; index++) {
+        const element = links[index];
+        if (element.className === "nav-link active"){
+            //Add pseudo-class "focus" to element with "nav-link active" class
+            element.focus
+        }
+    }
+});
