2756637d433978b8b083e47866d188cc2a17f5aa
Naomi Mango <naomi.mango@gmail.com>
2021-09-10 12:03:39 +0200

    [MAJ] Animations de la timeline

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // Finding Unintended Body Overflow
 var docWidth = document.documentElement.offsetWidth;
-
 [].forEach.call(
     document.querySelectorAll('*'),
     function(el) {
@@ -9,6 +8,36 @@ var docWidth = document.documentElement.offsetWidth;
         }
     }
 );
+
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        const firstTimeline = entry.target.querySelector('.training-2');
+        const secondTimeline = entry.target.querySelector('.training-1');
+        // If the elements are visible for bigger viewports
+        if (entry.isIntersecting && window.innerWidth > 991) {
+            // Add the animation classes
+            firstTimeline.classList.add('right-entry-animation');
+            secondTimeline.classList.add('left-entry-animation');
+            return; // if we added the classes, exit the function
+        }
+        // If the elements are visible for smaller viewports
+        if (entry.isIntersecting && window.innerWidth <= 991) {
+            // Add the animation classes
+            firstTimeline.classList.add('top-entry-animation');
+            secondTimeline.classList.add('top-entry-animation');
+            return; // if we added the classes, exit the function
+        }
+        // We're not intersecting, so remove the classes!
+        firstTimeline.classList.remove('right-entry-animation');
+        secondTimeline.classList.remove('left-entry-animation');
+        firstTimeline.classList.remove('top-entry-animation');
+        secondTimeline.classList.remove('top-entry-animation');
+    });
+});
+// Tell the observer which element to track
+observer.observe(document.querySelector('.main-timeline'));
 
 const links = document.links;
 window.addEventListener('scroll', function(e)  {
